12a2fc80fa9f9866c815cec18f6f2dfead337ee0
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2025-05-07 01:33:16 +0530

    add new controller and route for submissions

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import problemRoutes from "./routes/problem.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import executionRoutes from "./routes/executeCode.routes.js";
+import submissionRoutes from "./routes/submission.routes.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/execute-code", executionRoutes);
+app.use("/api/v1/submission", submissionRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 8080");
