44da7a62611685733c9020964d4e96a860712b66
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2025-05-12 17:31:56 +0530

    react router dom setup and basic routes setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-extrabold text-purple-500 flex justify-center items-center h-screen">
-        Welcome to LeetNest
-      </h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </div>
   );
 };
