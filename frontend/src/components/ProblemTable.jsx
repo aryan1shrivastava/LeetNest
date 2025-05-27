cb5598cad997026b251de09b2aeb463a5c8c7f36
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2025-05-27 19:13:21 +0530

    updated the home page and all problem table on the homepage

--- frontend/src/components/ProblemTable.jsx
+++ frontend/src/components/ProblemTable.jsx
@@ -132,7 +132,7 @@ const ProblemsTable = ({ problems }) => {
           <tbody>
             {paginatedProblems.length > 0 ? (
               paginatedProblems.map((problem) => {
-                const isSolved = problem.solvedBy.some(
+                const isSolved = problem.solvedBy?.some(
                   (user) => user.userId === authUser?.id
                 );
                 return (
