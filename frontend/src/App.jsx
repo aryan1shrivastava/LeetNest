9543c7cc5e2cee2bba02e46b93ca09c8a7638af9
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2026-01-15 23:33:41 +0530

    updated the auth logic at load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   }, [checkAuth]);
 
 
-  if (isCheckingAuth && !authUser) {
+  if (isCheckingAuth) {
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
